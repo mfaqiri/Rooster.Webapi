a40f4adc37aafbdcc8f1cf07397a32b6fea307a7
Muhammad Aman <maman0022@outlook.com>
2021-05-25 14:30:33 -0500

    added unitofwork to controller

--- Rooster.Client/Controllers/ErrandController.cs
+++ Rooster.Client/Controllers/ErrandController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Rooster.Storing
 
 namespace Rooster.Client.Controllers
 {
@@ -11,10 +12,17 @@ namespace Rooster.Client.Controllers
     [Route("[controller]")]
     public class ErrandController : ControllerBase
     {
+        private UnitOfWork _unitOfWork { get; set; }
+
+        public ErrandController(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork
+        }
+
         [HttpGet]
         public void Get()
         {
-            
+            return 'Hello'
         }
     }
 }
