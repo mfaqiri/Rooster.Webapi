b397f0b12d8b4ba60e642c1dc64cfd53249576c5
Muhammad Aman <maman0022@outlook.com>
2021-05-27 16:14:26 -0500

    added user service

---
+++ Rooster.Client/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Rooster.Domain.Models;
+using Rooster.Storing;
+
+namespace Rooster.Client.Controllers
+{
+  [ApiController]
+  [Route("[controller]")]
+  public class UserController : ControllerBase
+  {
+    private UnitOfWork _unitOfWork { get; set; }
+    public UserController(UnitOfWork unitOfWork)
+    {
+      _unitOfWork = unitOfWork;
+    }
+
+    [HttpGet]
+    [EnableCors("MVC")]
+    public ActionResult Get()
+    {
+      return Unauthorized();
+    }
+
+    [HttpPost]
+    [EnableCors("MVC")]
+    public ActionResult Add(User User)
+    {
+      if (User.Email == null)
+      {
+        return BadRequest("Email is required");
+      }
+      _unitOfWork.Users.Insert(User);
+      _unitOfWork.Save();
+      return Ok();
+    }
+  }
+}
